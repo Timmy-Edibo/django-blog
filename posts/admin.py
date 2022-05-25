ec1d99048e53db51b7d469fca58050ae666f5aca
timmy-edibo <timothyedibo@gmail.com>
2022-05-25 02:14:59 +0100

    configured the admin interface

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from . import models
+# admin.site.register(Todo)
+
+@admin.register(models.Post)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['author', 'title', 'body', 'created_at']
+    list_per_page = 10
+    search_fields = ['title']
